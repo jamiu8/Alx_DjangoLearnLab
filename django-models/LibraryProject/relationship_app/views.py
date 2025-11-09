4dc52a4da8ef3852a96555bbe4f5707ec6b608fe
jamiu <amurejamiu8@gmail.com>
2025-11-09 17:02:03 +0100

    good

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from relationship_app.models import Book, Author, Library
+from .models import Book, Library
 from django.views.generic import DetailView
 
 # Create your views here.
