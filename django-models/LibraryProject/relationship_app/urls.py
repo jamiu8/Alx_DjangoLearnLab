3d9324a7a6c89a50db92005b3a5acdcc8964de4a
jamiu <amurejamiu8@gmail.com>
2025-11-09 18:50:51 +0100

    good

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path("admin-view/", views.admin_view, name="admin_view"),
     path("librarian-view/", views.librarian_view, name="librarian_view"),
     path("member-view/", views.member_view, name="member_view"),
-    path("book/add/", views.add_book, name="add_book"),
-    path("book/<int:pk>/edit/", views.edit_book, name="edit_book"),
-    path("book/<int:pk>/delete/", views.delete_book, name="delete_book"),
+    path("add_book/", views.add_book, name="add_book"),         
+    path("edit_book/<int:pk>/", views.edit_book, name="edit_book"), 
+    path("delete_book/<int:pk>/", views.delete_book, name="delete_book"),
 ]
