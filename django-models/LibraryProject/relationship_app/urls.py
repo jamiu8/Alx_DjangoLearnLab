7bb4611d880f51ead95a2338aa92274791d8f915
jamiu <amurejamiu8@gmail.com>
2025-11-09 18:48:12 +0100

    good

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -11,7 +11,10 @@ urlpatterns = [
     path("login/", LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
     path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
     path("register/", views.register_view, name="register"),  # Keep function-based registration
-     path("admin-view/", views.admin_view, name="admin_view"),
+    path("admin-view/", views.admin_view, name="admin_view"),
     path("librarian-view/", views.librarian_view, name="librarian_view"),
     path("member-view/", views.member_view, name="member_view"),
+    path("book/add/", views.add_book, name="add_book"),
+    path("book/<int:pk>/edit/", views.edit_book, name="edit_book"),
+    path("book/<int:pk>/delete/", views.delete_book, name="delete_book"),
 ]
