876a1eb582837c10dce1bc55f81d1ddfeca3a57b
jamiu <amurejamiu8@gmail.com>
2025-11-09 16:53:26 +0100

    good

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,13 +1,13 @@
 from relationship_app.models import Book, Library, Librarian, Author
 
-authe = Author.objects.get(name=author_name)
-authe.objects.filter(author=author)
+authe = Author.objects.get(name='')
+authe.objects.filter(author="author")
 
 
-book = Library.objects.get(name=library_name)
+book = Library.objects.get(name='')
 book.books.all()
 
-# Library.objects.values_list("book", flat=True)
+# Library.objects.values_list("book", flat=True)S
 
 Librarian.objects.get(library= "s")
 
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from relationship_app.models import Book, Author, Library
+from django.views.generic import DetailView
 
 # Create your views here.
+def list_books(request):
+    books = Book.objects.all()
+    context = {"books": books}
+    return render(request, "list_books.html", context)
+
+
+# Class-based View — display details of a specific library
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = "library_detail.html"
+    context_object_name = "library"
