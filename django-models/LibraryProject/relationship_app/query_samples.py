722656cc05f83d76c8232cbbd2e775291aa6935b
jamiu <amurejamiu8@gmail.com>
2025-11-09 15:47:01 +0100

    good

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -3,11 +3,11 @@ from relationship_app.models import Book, Library, Librarian
 Book.objects.filter(author = "salmon")
 
 books = Library.objects.get(name="library_name")
-books.book.all()
+books.objects.all()
 
 # Library.objects.values_list("book", flat=True)
 
 Libr = Library.objects.get(name="library_name")
 
-Libr.book.all()
+Libr.objects.all()
 
