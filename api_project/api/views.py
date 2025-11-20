b7e011f8f4119a5f29a1ef39a9d33a285224216e
jamiu <amurejamiu8@gmail.com>
2025-11-20 09:15:33 +0100

    everything gucci

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,10 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
 
 class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+# New ViewSet for full CRUD
+class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
