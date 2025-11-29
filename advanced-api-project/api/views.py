f70b33dcf95ae4abb94357e6d4d0759aa0edab20
jamiu <amurejamiu8@gmail.com>
2025-11-29 19:18:16 +0100

    message

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,8 +1,5 @@
 from rest_framework import generics
-from rest_framework.permissions import (
-    IsAuthenticatedOrReadOnly,
-    IsAuthenticated
-)
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
 # Create your views here.
